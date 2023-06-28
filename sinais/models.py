eec745c15efcd58a4a2f17781b6a062f9dc25b5f
admin <esleynathan@hotmail.com>
2023-06-28 20:14:21 -0300

    Django Signals | Organizando os signals

--- sinais/models.py
+++ sinais/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.db.models.signals import post_save
-
-from django.dispatch import receiver
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=50)
@@ -15,14 +12,4 @@ class Historico(models.Model):
     nome = models.CharField(max_length=50)
     email = models.EmailField()
     telefone = models.CharField(max_length=20)
-    pessoa = models.ForeignKey(Pessoa, on_delete=models.DO_NOTHING)
-    
-@receiver(post_save, sender=Pessoa)
-def envia_sinal(sender, instance, created, **kwargs):
-    x = Historico(nome=instance.nome,
-                email=instance.email,
-                telefone=instance.telefone,
-                pessoa=instance
-                )
-    x.save()
-    print('Histórico cadastrado')
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.DO_NOTHING)   
