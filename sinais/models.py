2cdf4e82bc97d06f6064435b22d6c4fbc6cc4b32
admin <esleynathan@hotmail.com>
2023-06-28 20:03:51 -0300

    Django Signals | Colocando em prática

--- sinais/models.py
+++ sinais/models.py
@@ -8,14 +8,20 @@ class Pessoa(models.Model):
 
     def __str__(self) -> str:
         return self.nome
+
+class Historico(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.DO_NOTHING)
     
 def envia_sinal(sender, instance, created, **kwargs):
-    if created:
-        print('Estou sendo criada.')
-    else:
-        print('Estou sendo alterada.')
-        print(instance.nome)
-        print(instance.email)        
-        print(instance.telefone)
+    x = Historico(nome=instance.nome,
+                email=instance.email,
+                telefone=instance.telefone,
+                pessoa=instance
+                )
+    x.save()
+    print('Histórico cadastrado')
 
 post_save.connect(envia_sinal, sender=Pessoa)
